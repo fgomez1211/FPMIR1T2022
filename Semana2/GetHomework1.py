9e76b32373e1abc2a0a93698068a603df3bc08cc
fgomez1211 <98927828+fgomez1211@users.noreply.github.com>
2022-02-08 22:59:48 -0600

    Finalización GetHomework1, Python y Jupyter.

--- Semana2/GetHomework1.py
+++ Semana2/GetHomework1.py
@@ -1,13 +1,18 @@
 
-
-from ast import Return
-from typing import Text
+from hashlib import new
 import webbrowser
 
-
-def GetHomework(SemanaN):
-    print(f"Semana{SemanaN}")
+Num =input("Ingrese la semana")          #Ingresar el número de semana y se almacena en la variable Num
 
 
-GetHomework('1')
+def GetHomework(SemanaN):               #Definición de la función GetHomework con el parámetro "SemanaN"
+    return(f"Semana{SemanaN}")
+    
+GetHomework(Num)                        #Ejecución de la función GetHomwork con "Num"
 
+Semana = str(GetHomework(Num))          
+link = "https://fpmir.azurewebsites.net/api/AZFMIR?AZFNUM="
+
+homework = link+Semana                  #Concatenación en variable "homework"
+
+webbrowser.open(homework, new=2)        #Ejecución de interfaz webbrowser.2
