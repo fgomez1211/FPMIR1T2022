39774d58bc7d0302c442fa9dfe3b4eeda2761061
Fernando Gomez <fgomez1211@gmail.com>
2022-03-03 22:36:53 -0600

    Upload Track2 hasta Ejercicio No.3

---
+++ PythonBootCamp/Track2.py
@@ -0,0 +1,33 @@
+
+#Track Ciclos Python
+
+
+#Práctica No.1
+"""
+Num1=int(input("Ingrese un numero: "))
+for x in range (Num1+1,0,-1):
+    Num2=x-1
+    for i in range(Num2,-1, -1):
+        if i==0: 
+            break
+        else:
+            print(i, end=" ")
+    print("")
+"""
+
+
+#Práctica No.2
+"""
+Num1 = (input("Ingrese un número: "))
+x = 0
+try:
+    i = int(Num1)
+    while i > 0:
+        y = i % 10
+        x = (x * 10) + y
+        i //= 10
+    print('El inverso del número ingresado es: ', x)
+except:
+    pass
+"""
+
