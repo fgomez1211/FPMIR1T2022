f6aab6731666f8bc9e56dde6112e6814981000c1
Fernando Gomez <fgomez1211@gmail.com>
2022-03-03 20:36:39 -0600

    Se ha dejado el codigo sin comentarios.

--- PythonBootCamp/Track1.py
+++ PythonBootCamp/Track1.py
@@ -1,7 +1,8 @@
 
 #Track de Condiciones
+
+
 #Ejercicio No.1
-"""
 Entrada=input("Ingrese un caracter alfanumerico: ")
 
 vocales=["a","e","i","o","u"]
@@ -9,11 +10,10 @@ if Entrada in vocales:
     print("El caracter",Entrada," es una vocal")
 else:
     print("El caracter",Entrada," es una consonante")
-"""
+
 
 
 #Ejercicio No.2
-"""
 Lado1=input("Ingrese longitud 1 del triangulo: ")
 Lado2=input("Ingrese longitud 2 del triangulo: ")
 Lado3=input("Ingrese longitud 3 del triangulo: ")
@@ -24,18 +24,16 @@ elif Lado1!=Lado2!=Lado3:
     print("El triangulo es Escaleno")
 elif Lado1==Lado2==Lado3:
     print("El triangulo es Equilatero")
-"""
+
 
 
 #Ejercicio No.3
-"""
 NotaAlumno=int(input("Ingrese la nota del alumno: "))
 print("El estudiante aprobó con una nota de ", NotaAlumno) if (NotaAlumno>=65) else print("El estudiante reprobó con una nota de ",NotaAlumno)
-"""
+
 
 
 #Ejercicio No.4
-"""
 Num1=int(input("Ingrese numero del mes: "))
 Meses = {
     1:"Enero",
@@ -52,11 +50,10 @@ Meses = {
     12:"Diciembre"
 }
 print("El més",Num1," es ",Meses.get(Num1,"un més invalido"))
-"""
+
 
 
 #Ejercicio No.5
-"""
 AñoIngresado=int(input("Ingrese un año: "))
 if AñoIngresado%100 and AñoIngresado%400 ==0:
     print("El año ",AñoIngresado," es un año bisiesto")
@@ -64,4 +61,4 @@ elif AñoIngresado%100!=0 and AñoIngresado%4==0:
      print("El año ",AñoIngresado," es un año bisiesto")
 else:
     print("El año ", AñoIngresado," no es bisiesto")
-"""
+
